d792ed85755177f22a8839f2d1ca223439bfec7d
grovem <matthewg74@gmail.com>
2020-03-25 19:32:33 -0600

    pushing code for authentication

---
+++ config/passport-config.js
@@ -0,0 +1,66 @@
+const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const TwitterStrategy = require('passport-twitter').Strategy;
+const bcrypt = require('bcrypt');
+
+function initialize(passport, getUserByEmail) {
+    const authenticateUser = async (email, password, done) => {
+        const user = getUserByEmail(email);
+        if (user === null) {
+            return done(null, false, { message: 'That email address is not registered.' });
+        }
+
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Password is incorrect.' });
+            }
+        } catch (e) {
+            return done(e);
+        }
+    };
+    passport.use(new LocalStrategy({ usernameField: 'email' },
+        authenticateUser));
+
+    passport.serializeUser((user, done) => done(null, user.id));
+    passport.deserializeUser((id, done) => {
+        return done(null, false);
+    });
+
+    passport.use(new GoogleStrategy({
+        callbackURL: 'http://localhost:8080/auth/google/cb',
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('passport Google callback function fired:');
+        return done(null, console.log(profile));
+    }
+    ));
+
+    passport.use(new FacebookStrategy({
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        callbackURL: 'http://localhost:8080/auth/facebook/cb'
+    },
+        function (accessToken, refreshToken, profile, done) {
+            console.log('passport Facebook callback function fired:');
+            return done(null, console.log(profile));
+        }
+    ));
+
+    passport.use(new TwitterStrategy({
+        consumerKey: process.env.TWITTER_API_KEY,
+        consumerSecret: process.env.TWITTER_API_SECRET_KEY,
+        callbackURL: 'http://localhost:8080/auth/twitter/cb'
+    },
+        function (token, tokenSecret, profile, done) {
+            console.log('passport Twitter callback function fired:');
+            return done(null, console.log(profile));
+        }
+    ));
+
+}
+
+module.exports = initialize;
--- server.js
+++ server.js
@@ -1,25 +1,146 @@
+// // Load environment variables and set inside process.env
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+// // dependencies
 const express = require("express");
-
-const app = express();
+const exphbs = require("express-handlebars");
 const PORT = process.env.PORT || 8080;
+const app = express();
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override")
+const bcrypt = require("bcrypt");
 
-//Need database
-var db = require("");
+const initializePassport = require("./config/passport-config");
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const users = [];
 
-// Static directory
-app.use(express.static("public"));
+// setup view engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-//Routes
-var routes = require('./routes');
-app.use(routes);
+app.use(express.static("public")); 
+
+app.use(express.urlencoded({ extended: false }));
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,  // do not resave if nothing has changed
+    saveUninitialized: false,  // do not save empty values
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+
+// routes
+app.get("/", checkAuthenticated, (req, res) => {
+    res.render("index", { name: req.user.name })
+});
+
+app.get("/login", checkNotAuthenticated, (req, res) => {
+    res.render("login")
+});
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+    res.render("register")
+});
+
+app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
+    successRedirect: "/success",
+    failureRedirect: "/login",
+    failureFlash: true
+}));
+
+app.post("/register", checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10) // hash user password 10 times
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        res.redirect("/login");
+    } catch {
+        res.redirect("/register");
+    }
+    console.log(users);
+})
+
+app.delete("/logout", (req, res) => {
+    req.logOut();
+    res.redirect("/");
+})
 
 
-db.sequelize.sync({ force: true }).then(function () {
-    app.listen(PORT, function () {
-        console.log(`App is now listening on ${PORT}`);
-    });
+// auth routes
+
+// authenticate with Google
+app.get("/auth/google", passport.authenticate("google", {
+    scope: ["profile"]
+}));
+
+app.get("/auth/google/cb",
+    passport.authenticate("google", {
+        successRedirect: "/success",
+        failureRedirect: "/"
+    }));
+
+// authenticate with Facebook
+app.get("/auth/facebook", passport.authenticate("facebook", {
+}));
+
+app.get("/auth/facebook/cb",
+    passport.authenticate("facebook", {
+        successRedirect: "/success",
+        failureRedirect: "/"
+    }));
+
+// authenticate with Twitter
+app.get("/auth/twitter", passport.authenticate("twitter", {
+}));
+
+app.get("/auth/twitter/cb",
+    passport.authenticate("twitter", {
+        successRedirect: "/success",
+        failureRedirect: "/"
+    }));
+
+app.get("/success", checkNotAuthenticated, (req, res) => {
+    res.render("success")
+});
+
+app.delete("/auth/logout", (req, res) => {
+    req.logOut();
+    res.redirect("/");
+})
+
+// Redirect unauthenticated user to login page if authentication required
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect("/");
+    }
+    next();
+}
+
+// starting Express app
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
