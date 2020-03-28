0c5ea7cdb021df659f53d9cc5fe6180fb277da21
Jennifer Mullin <jennifer.lindsey.mullin@gmail.com>
2020-03-28 14:24:50 -0600

    fixed passport issue and added pseudo code for integrating database

--- routes/html/html-routes.js
+++ routes/html/html-routes.js
@@ -2,7 +2,8 @@ const bcrypt = require("bcrypt");
 const router = require("express").Router();
 const passport = require("passport");
 
-const users = [];
+
+const users = require("../../server");
 
 // routes
 router.get("/", checkAuthenticated, (req, res) => {
@@ -38,70 +39,20 @@ router.post("/login", checkNotAuthenticated, passport.authenticate("local", {
 router.post("/register", checkNotAuthenticated, async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10) // hash user password 10 times
+        //create call to database here
         users.push({
             id: Date.now().toString(),
             name: req.body.name,
             email: req.body.email,
             password: hashedPassword
         });
-        res.redirect("/login");
+        console.log("should be redirecting to /");
+        return res.redirect("/login");
     } catch {
-        res.redirect("/register");
+        console.log("should be redirecting to /register");
+        return res.redirect("/register");
     }
-    console.log(users);
-})
-
-router.delete("/logout", (req, res) => {
-    req.logOut();
-    res.redirect("/");
-})
-
-
-// routes
-router.get("/", checkAuthenticated, (req, res) => {
-    res.render("index", { name: req.user.name })
-});
-
-router.get("/map", (req, res) => {
-    res.render("map");
-});
-
-router.get("/tips", (req, res) => {
-    res.render("tips");
-});
-
-router.get("/profile", (req, res) => {
-    res.render("profile");
-});
-
-router.get("/login", checkNotAuthenticated, (req, res) => {
-    res.render("login")
-});
-
-router.get("/register", checkNotAuthenticated, (req, res) => {
-    res.render("register")
-});
-
-router.post("/login", checkNotAuthenticated, passport.authenticate("local", {
-    successRedirect: "/success",
-    failureRedirect: "/login",
-    failureFlash: true
-}));
-
-router.post("/register", checkNotAuthenticated, async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10) // hash user password 10 times
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        });
-        res.redirect("/login");
-    } catch {
-        res.redirect("/register");
-    }
-    console.log(users);
+    console.log("this is local strategy", users);
 })
 
 router.delete("/logout", (req, res) => {
@@ -163,6 +114,8 @@ function checkAuthenticated(req, res, next) {
 }
 
 function checkNotAuthenticated(req, res, next) {
+    console.log(req.body);
+    
     if (req.isAuthenticated()) {
         return res.redirect("/");
     }
